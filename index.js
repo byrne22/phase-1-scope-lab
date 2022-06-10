f6002260376f88bd0798ab0043ceb38fda091598
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-06-10 15:03:03 -0400

    commit tree 3d898074739f526c4320d8dd88a2e1fea73a9c77

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+
 // Write your solution in this file!
+// const globalVar = 1;
+
+// function firstFunc() {
+//   const firstVar = 2;
+
+//   return firstVar + globalVar;
+// }
+
+// firstFunc();
+// // => 3
+
+var customerName= "bob";
+
+function upperCaseCustomerName(){
+
+    return customerName = customerName.toUpperCase()    
+}
+// console.log(upperCaseCustomerName());
+
+function setBestCustomer(){
+    bestCustomer='not bob'
+   
+}
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob'
+}
+
+const leastFavoriteCustomer= 'jay'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='bob'
+}
